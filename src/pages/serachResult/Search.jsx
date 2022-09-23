902e17336fb24cd96cc1d7015cd6e7665a0f6ab3
Martin_Ma <dingma665@gmail.com>
2022-09-23 01:54:11 -0400

    test

---
+++ src/pages/serachResult/Search.jsx
@@ -0,0 +1,28 @@
+import React, { Fragment, useState } from 'react'
+import Header from '../../components/header/Header'
+import ProductList from '../../components/productList/ProductList'
+import ProductModal from '../../components/productModal/ProductModal'
+
+const Search = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+    const handleModalOpen = () => setModalOpen(true);
+    const handleModalClose = () => setModalOpen(false);
+
+    return (
+        <Fragment>
+            <Header />
+            <ProductList handleModalOpen={handleModalOpen} />
+            <ProductModal open={modalOpen} handleModalClose={handleModalClose} />
+        </Fragment>
+    )
+}
+
+export default Search
+
+
+
+
+
+
+
+
